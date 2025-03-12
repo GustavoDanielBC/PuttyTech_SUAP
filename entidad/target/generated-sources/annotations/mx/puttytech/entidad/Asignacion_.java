af22e3db2b011d06609e14f825e6057218b53930
GustavoDanielBC <gustavo.barrios@uabc.edu.mx>
2025-03-12 02:06:48 -0700

    Ajustes de inicio de commits

---
+++ entidad/target/generated-sources/annotations/mx/puttytech/entidad/Asignacion_.java
@@ -0,0 +1,17 @@
+package mx.puttytech.entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.puttytech.entidad.Profesor;
+import mx.puttytech.entidad.UnidadAprendizaje;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-12T01:54:59")
+@StaticMetamodel(Asignacion.class)
+public class Asignacion_ { 
+
+    public static volatile SingularAttribute<Asignacion, Profesor> numProfesor;
+    public static volatile SingularAttribute<Asignacion, UnidadAprendizaje> claveUnidadAprendizaje;
+    public static volatile SingularAttribute<Asignacion, Integer> idAsignacion;
+
+}
---
+++ entidad/target/generated-sources/annotations/mx/puttytech/entidad/UnidadAprendizaje_.java
@@ -0,0 +1,20 @@
+package mx.puttytech.entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.puttytech.entidad.Asignacion;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-12T01:54:59")
+@StaticMetamodel(UnidadAprendizaje.class)
+public class UnidadAprendizaje_ { 
+
+    public static volatile SingularAttribute<UnidadAprendizaje, Integer> horasLaboratorio;
+    public static volatile SingularAttribute<UnidadAprendizaje, String> nombreunidad;
+    public static volatile ListAttribute<UnidadAprendizaje, Asignacion> asignacionList;
+    public static volatile SingularAttribute<UnidadAprendizaje, Integer> claveUnidadAprendizaje;
+    public static volatile SingularAttribute<UnidadAprendizaje, Integer> horasClase;
+    public static volatile SingularAttribute<UnidadAprendizaje, Integer> horasTaller;
+
+}
