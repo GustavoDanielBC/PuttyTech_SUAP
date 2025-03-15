d5cd98ae47f55429246912ef29fc153e65e5f485
GustavoDanielBC <gustavo.barrios@uabc.edu.mx>
2025-03-15 01:26:28 -0700

    PBI_PROF_CONS US4 Diseñar la interfaz de usuario para el modulo de consultas de profesores
    Se agrego una tabla de consulta de profesores para ver a los profesores registrados.

---
+++ entidad/target/generated-sources/annotations/mx/puttytech/entidad/Profesor_.java
@@ -0,0 +1,21 @@
+package mx.puttytech.entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.puttytech.entidad.Asignacion;
+import mx.puttytech.entidad.Usuario;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-03-15T01:04:17")
+@StaticMetamodel(Profesor.class)
+public class Profesor_ { 
+
+    public static volatile ListAttribute<Profesor, Usuario> usuarioList;
+    public static volatile SingularAttribute<Profesor, Integer> numProfesor;
+    public static volatile SingularAttribute<Profesor, String> apellido;
+    public static volatile ListAttribute<Profesor, Asignacion> asignacionList;
+    public static volatile SingularAttribute<Profesor, String> nombre;
+    public static volatile SingularAttribute<Profesor, String> rfc;
+
+}
